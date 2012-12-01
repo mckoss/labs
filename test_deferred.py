e8b68b7329be660f76dc12e38547b4257320ad94
Mike Koss <mckoss@startpad.org>
2012-12-01 08:43:43 -0800

    test ran

--- test_deferred.py
+++ test_deferred.py
@@ -18,14 +18,17 @@ class AsyncObject(object):
 class TestDeferred(unittest.TestCase):
     def setUp(self):
         def on_resolve(*args, **kwargs):
+            self.resolve_ran = True
             self.assertEqual(args, ['resolve'])
             self.assertEqual(kwargs, {'finish_type': 'resolve'})
 
         def on_error(*args, **kwargs):
+            self.error_ran = True
             self.assertEqual(args, ['error'])
             self.assertEqual(kwargs, {'finish_type': 'error'})
 
         def on_always(*args, **kwargs):
+            self.always_ran = True
             self.assertEqual(len(args), 1)
             self.assertIn(args[0], ('resolve', 'error'))
             self.assertEqual(len(kwargs), 1)
@@ -33,6 +36,9 @@ class TestDeferred(unittest.TestCase):
 
         self.d = Deferred()
         self.d.then(on_resolve, error=on_error, always=on_always)
+        self.resolve_ran = False
+        self.error_ran = False
+        self.always_ran = False
 
     def test_create(self):
         self.assertIsNotNone(self.d)
@@ -41,11 +47,17 @@ class TestDeferred(unittest.TestCase):
         self.d.resolve('resolve', finish_type='resolve')
         self.assertTrue(self.d.finished)
         self.assertTrue(self.d.is_resolved)
+        self.assertTrue(self.resolve_ran)
+        self.assertFalse(self.error_ran)
+        self.assertTrue(self.always_ran)
 
     def test_error(self):
         self.d.reject('error', finish_type='error')
         self.assertTrue(self.d.finished)
         self.assertFalse(self.d.is_resolved)
+        self.assertFalse(self.resolve_ran)
+        self.assertTrue(self.error_ran)
+        self.assertTrue(self.always_ran)
 
 
 if __name__ == '__main__':
