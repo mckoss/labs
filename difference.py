7f45cb2e8c05d7e2852bd158a1c62449f9b8445f
Mike Koss <mckoss@startpad.org>
2012-12-15 18:26:38 -0800

    faster version

--- difference.py
+++ difference.py
@@ -9,6 +9,39 @@ def main():
         print ds
 
 def find_difference_set(k):
+    m = k * (k - 1) + 1
+    s = [0, 1] + [0] * (k - 2)
+    stack = [set()]
+    next = 1
+
+    def test_number(n):
+        d = stack[-1].copy()
+        for i in range(next):
+            if (n - s[i]) % m in d or (s[i] - n) % m in d:
+                return None
+            d.add((n - s[i]) % m)
+            d.add((s[i] - n) % m)
+        return d
+
+    while True:
+        d = test_number(s[next])
+        if d is not None:
+            if next == k - 1:
+                return s
+            stack.append(d)
+            s[next + 1] = s[next] + 2
+            next += 1
+            continue
+        s[next] += 1
+        if s[next] >= m:
+            s[next - 1] += 1
+            stack.pop()
+            next -= 1
+            if next == 1:
+                return ()
+
+def slow_find_difference_set(k):
+    """ This one is concise, and expressive - but SLOW """
     if k in (7,):
         return ()
     m = k * (k - 1) + 1
