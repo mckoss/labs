0c3b186eedf707c10b267677c971755936b17162
Mike Koss <mckoss@startpad.org>
2013-01-01 12:08:01 -0800

    passing - backtracking search

--- search.py
+++ search.py
@@ -1,9 +1,10 @@
 from progress import Progress
 
 
-class SearchSpace(object):
+class RestartSearch(object):
     """
-    Abstract class defining a solution-space search (with backtracking).
+    Abstract class defining a solution-space search.  When an invalid configuration is found
+    it restarts the search providing the next sequence of choices.
 
     Simplifiies implementation by:
 
@@ -44,15 +45,13 @@ class SearchSpace(object):
     def search(self):
         pass
 
-    def reset(self):
-        self.depth = 0
-
     def complete(self):
         pass
 
     def run(self):
         while self.depth >= 0:
-            self.reset()
+            self.depth = 0
+            self.step()
             result = self.search()
             if result is not None:
                 self.complete()
@@ -61,14 +60,81 @@ class SearchSpace(object):
         self.complete()
 
 
-class SearchProgress(SearchSpace):
+class SearchProgress(object):
     def __init__(self, name='Search', report_rate=5):
         super(SearchProgress, self).__init__()
         self.progress = Progress(name=name, report_rate=report_rate)
 
-    def reset(self):
-        super(SearchProgress, self).reset()
+    def step(self):
         self.progress.report(self.choices)
 
     def complete(self):
+        super(SearchProgress, self).complete()
         self.progress.report(self.choices, final=True)
+
+
+class BacktrackSearch(object):
+    """
+    Abstract class defining a solution-space search (with backtracking).
+
+    Simplifiies implementation by:
+
+    - Organizing searching with backtracking.
+    - Starting searches from known starting points.
+    - Limiting searches to specified ending points.
+    - Organizing workers to cooperatively search a space w/o overlap.
+
+    All choices must be modeled as sequence of integers (from 0 to some maximum).
+    """
+    def __init__(self):
+        self.choices = []
+        self.limits = []
+        self.restart()
+
+    def restart(self):
+        self.depth = 0
+
+    def run(self):
+        while not self.is_finished():
+            result = self.step()
+            if result is not None:
+                self.complete()
+                return result
+        self.complete()
+
+    def is_finished(self):
+        # TODO: Add start a limit sequences.
+        return self.depth < 0
+
+    def choose(self, limit):
+        if self.depth == len(self.choices):
+            self.limits.append(limit)
+            value = 0
+            self.choices.append(value)
+        else:
+            value = self.choices[self.depth]
+        self.depth += 1
+        return value
+
+    def reject(self):
+        """ Revert the last choice and prepare the next one. """
+        latest = self.depth - 1
+        while self.depth >= 0:
+            self.depth -= 1
+            if self.depth == -1:
+                break
+            if self.depth < latest:
+                self.backtrack(self.choices[self.depth])
+            self.choices[self.depth] += 1
+            if self.choices[self.depth] < self.limits[self.depth]:
+                break
+
+        del self.choices[self.depth + 1:]
+        del self.limits[self.depth + 1:]
+
+    def complete(self):
+        pass
+
+    def backtrack(self, choice):
+        """ If backtracking is not supported - a restart from the beginning works. """
+        self.restart()
--- test_search.py
+++ test_search.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python
 import unittest
 
-from search import SearchProgress
+from search import RestartSearch, BacktrackSearch, SearchProgress
 
 
 class TestSearch(unittest.TestCase):
-    def test_queens(self):
-        tests = [(1, [(0, 0)]),
-                 (2, None),
-                 (3, None),
-                 (4, [(0, 1), (1, 3), (2, 0), (3, 2)]),
-                 (5, [(0, 0), (1, 2), (2, 4), (3, 1), (4, 3)]),
-                 (6, [(0, 1), (1, 3), (2, 5), (3, 0), (4, 2), (5, 4)]),
-                 (7, [(0, 0), (1, 2), (2, 4), (3, 6), (4, 1), (5, 3), (6, 5)]),
-                 (8, [(0, 0), (1, 4), (2, 7), (3, 5), (4, 2), (5, 6), (6, 1), (7, 3)]),
-                 (10, [(0, 0), (1, 2), (2, 5), (3, 7), (4, 9), (5, 4), (6, 8), (7, 1), (8, 3), (9, 6)]),
-                 ]
+    def setUp(self):
+        self.tests = [(1, [(0, 0)]),
+                      (2, None),
+                      (3, None),
+                      (4, [(0, 1), (1, 3), (2, 0), (3, 2)]),
+                      (5, [(0, 0), (1, 2), (2, 4), (3, 1), (4, 3)]),
+                      (6, [(0, 1), (1, 3), (2, 5), (3, 0), (4, 2), (5, 4)]),
+                      (7, [(0, 0), (1, 2), (2, 4), (3, 6), (4, 1), (5, 3), (6, 5)]),
+                      (8, [(0, 0), (1, 4), (2, 7), (3, 5), (4, 2), (5, 6), (6, 1), (7, 3)]),
+                      (10, [(0, 0), (1, 2), (2, 5), (3, 7), (4, 9), (5, 4), (6, 8), (7, 1), (8, 3), (9, 6)]),
+                      (18, [(0, 0), (1, 2), (2, 4), (3, 1), (4, 7), (5, 14), (6, 11), (7, 15), (8, 12), (9, 16),
+                            (10, 5), (11, 17), (12, 6), (13, 3), (14, 10), (15, 8), (16, 13), (17, 9)]),
+                      ]
         """
-        (18, [(0, 0), (1, 2), (2, 4), (3, 1), (4, 7), (5, 14), (6, 11), (7, 15), (8, 12), (9, 16),
-        (10, 5), (11, 17), (12, 6), (13, 3), (14, 10), (15, 8), (16, 13), (17, 9)]),
         (20, [(0, 0), (1, 2), (2, 4), (3, 1), (4, 3), (5, 12), (6, 14), (7, 11), (8, 17), (9, 19),
         (10, 16), (11, 8), (12, 15), (13, 18), (14, 7), (15, 9), (16, 6), (17, 13), (18, 5),
         (19, 10)]),
         """
-        for test in tests:
+
+    def test_queens(self):
+        for test in self.tests:
             q = Queens(test[0])
             self.assertEqual(q.run(), test[1])
-            if test[1] is not None:
-                print q
+            # if test[1] is not None:
+            #     print q
+
+    def test_backtrack_queens(self):
+        for test in self.tests:
+            q = BacktrackQueens(test[0])
+            self.assertEqual(q.run(), test[1])
 
 
-
-class Queens(SearchProgress):
+class Queens(SearchProgress, RestartSearch):
     def __init__(self, size=8):
         super(Queens, self).__init__()
         self.size = size
@@ -62,6 +68,58 @@ class Queens(SearchProgress):
             self.queens.append(place)
 
         return self.queens
+
+    def __str__(self):
+        """ Represent as a board. """
+        rows = []
+        for place in self.queens:
+            rows.append(' '.join(['X' if col == place[1] else '.' for col in range(self.size)]))
+        return '\n'.join(rows)
+
+
+
+class BacktrackQueens(SearchProgress, BacktrackSearch):
+    def __init__(self, size=8):
+        super(BacktrackQueens, self).__init__()
+        self.size = size
+
+    def restart(self):
+        super(BacktrackQueens, self).restart()
+        self.rows = set()
+        self.cols = set()
+        self.diag1 = set()
+        self.diag2 = set()
+        self.queens = []
+
+    def check(self, place):
+        if place[0] in self.rows or \
+                place[1] in self.cols or \
+                (place[0] - place[1]) in self.diag1 or  \
+                (place[0] + place[1]) in self.diag2:
+            return False
+
+        self.rows.add(place[0])
+        self.cols.add(place[1])
+        self.diag1.add(place[0] - place[1])
+        self.diag2.add(place[0] + place[1])
+        return True
+
+    def step(self):
+        super(BacktrackQueens, self).step()
+        place = (self.depth, self.choose(self.size))
+        if not self.check(place):
+            self.reject()
+            return
+        self.queens.append(place)
+        if self.depth == self.size:
+            return self.queens
+
+    def backtrack(self, choice):
+        place = self.queens.pop()
+        self.rows.remove(place[0])
+        self.cols.remove(place[1])
+        self.diag1.remove(place[0] - place[1])
+        self.diag2.remove(place[0] + place[1])
 
     def __str__(self):
         """ Represent as a board. """
