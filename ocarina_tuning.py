51349f8ac924c6af65d6d6ac3a778a0b94e27f4a
Mike Koss <mckoss@startpad.org>
2013-09-17 06:26:38 -0700

    major scale option

--- ocarina_tuning.py
+++ ocarina_tuning.py
@@ -11,20 +11,23 @@ from pprint import pprint
 from math import log
 
 STEP = pow(2.0, 1.0 / 12.0)
-IDEAL = [pow(STEP, i) for i in range(0, 12 + 1)]
+CHROMATIC = [pow(STEP, i) for i in range(0, 12 + 1)]
+MAJOR_INDEX = [0, 2, 4, 5, 7, 9, 11, 12]
+MAJOR = [CHROMATIC[MAJOR_INDEX[i]] for i in range(len(MAJOR_INDEX))]
 
 
 # Find best-fit solution to the ideal pitches from those given.
-def score(pitches):
+def score(pitches, standard):
     """
-    >>> score(IDEAL)
+    >>> score(CHROMATIC, MAJOR)
     0.0
-    >>> score([1.0, 2.0])
+    >>> score([1.0, 2.0], CHROMATIC)
     0.5672014747645435
+    >>> score(MAJOR, CHROMATIC)
+    0.016682396316604266
     """
-    pitches.sort()
-    nearest = [closest(pitch, pitches) for pitch in IDEAL]
-    scores = [pow(log(best / pitch), 2) for best, pitch in zip(nearest, IDEAL)]
+    nearest = [closest(pitch, pitches) for pitch in standard]
+    scores = [pow(log(best / pitch), 2) for best, pitch in zip(nearest, standard)]
     return sum(scores)
 
 
@@ -45,3 +48,9 @@ def closest(value, array):
         if abs(value - array[i]) < abs(value - array[i + 1]):
             return array[i]
     return array[len(array) - 1]
+
+
+def pitches_from_holes(holes):
+    """
+    Relative pitches base on hole area - stated in terms of ratio to fipple opening.
+    """
