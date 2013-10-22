67772c410f337156219809759c44224cbb54d5ea
Mike Koss <mckoss@startpad.org>
2013-10-21 21:49:03 -0700

    go fmt

--- difference.go
+++ difference.go
@@ -18,11 +18,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strconv"
-	"os"
-	"math"
-	"sort"
 	"io"
+	"math"
+	"os"
+	"sort"
+	"strconv"
 )
 
 const (
@@ -31,8 +31,8 @@ const (
 )
 
 func main() {
-	var err error;
-	var	start int = 2
+	var err error
+	var start int = 2
 	var end int = 103
 	var showHelp bool
 	var doContinue bool
@@ -57,61 +57,61 @@ func main() {
 		if err != nil || end < start || end > maxK {
 			fmt.Printf("'%s' is not a valid ending size.\n", flag.Arg(0))
 			usage()
-		}		
+		}
 	}
 
 	fmt.Printf("Searching for difference sets between %d and %d elements.\n", start, end)
 	powers := primePowers(maxK)
-    for i := 0; i < len(powers); i++ {
-    	ds := newDiffSet(powers[i] + 1)
+	for i := 0; i < len(powers); i++ {
+		ds := newDiffSet(powers[i] + 1)
 
-        if (ds.k < start) {
-            continue;
-        }
+		if ds.k < start {
+			continue
+		}
 
-        if (ds.k > end) {
-            break;
-        }
+		if ds.k > end {
+			break
+		}
 
-        ds.WriteInfo(os.Stderr)
-        ds.SearchNext()
-    }
+		ds.WriteInfo(os.Stderr)
+		ds.SearchNext()
+	}
 }
 
 func usage() {
-    fmt.Fprintf(os.Stderr, "Usage: difference [k-start] [k-end]]\n")
-    fmt.Fprintf(os.Stderr, "       difference [-c] [k] [prefix1 prefix 2 ...]\n")
-    fmt.Fprintf(os.Stderr, "Find difference sets of order k.\n\n")
-    flag.PrintDefaults()
+	fmt.Fprintf(os.Stderr, "Usage: difference [k-start] [k-end]]\n")
+	fmt.Fprintf(os.Stderr, "       difference [-c] [k] [prefix1 prefix 2 ...]\n")
+	fmt.Fprintf(os.Stderr, "Find difference sets of order k.\n\n")
+	flag.PrintDefaults()
 	os.Exit(1)
 }
 
 type diffSet struct {
-	k int
-	v int
-	trials int
-	prefixSize int
+	k           int
+	v           int
+	trials      int
+	prefixSize  int
 	targetDepth int
-	s []int
-	diffs []bool
+	s           []int
+	diffs       []bool
 }
 
 func newDiffSet(k int) *diffSet {
-	v := k * (k - 1) + 1
+	v := k*(k-1) + 1
 	return &diffSet{
-		k: k,
-		v: v,
-		s: make([]int, k),
-		diffs: make([]bool, v / 2),
+		k:     k,
+		v:     v,
+		s:     make([]int, k),
+		diffs: make([]bool, v/2),
 	}
 }
 
 func (ds *diffSet) WriteInfo(w io.Writer) {
-	fmt.Fprintf(w, "\nDifference set (k = %d, v = %d, lambda = 1):\n", ds.k, ds.v);
+	fmt.Fprintf(w, "\nDifference set (k = %d, v = %d, lambda = 1):\n", ds.k, ds.v)
 }
 
 func (ds *diffSet) SearchNext() {
-	
+
 }
 
 // primePowers returns 1 and all prime powers less than or equal to max.
@@ -122,11 +122,11 @@ func primePowers(max int) []int {
 
 	powers = append(powers, 1)
 	for i := 2; i < max; i++ {
-		if (s[i]) {
+		if s[i] {
 			continue
 		}
 		powers = append(powers, i)
-		if (i > sq) {
+		if i > sq {
 			continue
 		}
 		for j := i * i; j < max; j += i {
@@ -141,4 +141,3 @@ func primePowers(max int) []int {
 
 	return powers
 }
-
