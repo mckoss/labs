a913b743885e865072140b6a27de8f0c69f0b0e9
Mike Koss <mckoss@startpad.org>
2013-10-23 15:32:50 -0700

    failing test k=4

--- godiff/difference_test.go
+++ godiff/difference_test.go
@@ -38,6 +38,30 @@ func TestDiffSets_new(t *testing.T) {
 	expect(t, "s", ds.s, []int{0, 1, 0})
 }
 
+func TestDiffSets_Find3(t *testing.T) {
+	ds := newDiffSet(3, []int{0, 1})
+	ds.Find()
+	expect(t, "status", ds.status, Complete)
+	expect(t, "k", ds.k, 3)
+	expect(t, "v", ds.v, 7)
+	expect(t, "trials", ds.trials, 1)
+	expect(t, "targetDepth", ds.targetDepth, 0)
+	expect(t, "current", ds.current, 3)
+	expect(t, "s", ds.s, []int{0, 1, 3})
+}
+
+func TestDiffSets_Find4(t *testing.T) {
+	ds := newDiffSet(4, []int{0, 1})
+	ds.Find()
+	expect(t, "status", ds.status, Complete)
+	expect(t, "k", ds.k, 4)
+	expect(t, "v", ds.v, 13)
+	expect(t, "trials", ds.trials, 52)
+	expect(t, "targetDepth", ds.targetDepth, 0)
+	expect(t, "current", ds.current, 4)
+	expect(t, "s", ds.s, []int{0, 1, 3, 9})
+}
+
 func expect(t *testing.T, what string, got, expected interface{}) {
 	if !reflect.DeepEqual(got, expected) {
 		t.Errorf("Expected %s == %v but got %v.\n", what, expected, got)
