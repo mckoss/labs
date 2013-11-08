f98c7450ef0b889ceb20827d7f0b365fde21b7ff
Mike Koss <mckoss@startpad.org>
2013-11-08 15:22:21 -0800

    sample async jobs process

---
+++ jobs/jobs.go
@@ -0,0 +1,78 @@
+package jobs
+
+import (
+	"log"
+)
+
+type Rule struct {
+	target  string
+	prereqs []string
+	recipe  string
+}
+
+type Node struct {
+	name     string
+	children []*Node
+	parents  []*Node
+	rule     *Rule
+}
+
+type Tree struct {
+	lookup map[string]*Node
+	root   *Node
+}
+
+func NewTree() *Tree {
+	return &Nodes{lookup: make(map[string]*Node)}
+}
+
+func BuildTree(rules []*Rule) *Tree {
+	tree := NewTree()
+	for _, rule := range rules {
+		tree.AddRule(rule)
+	}
+
+	var roots []*Node
+	for _, node := range nodes.lookup {
+		if len(node.parents) == 0 {
+			roots = append(roots, node)
+		}
+	}
+
+	switch len(roots) {
+	case 0:
+		log.Printf("Circular reference in tree (at root).")
+	case 1:
+		tree.root = roots[0]
+	default:
+		var prereqs []string
+		for _, node := range roots {
+			prereqs = append(prereqs, node.name)
+		}
+		virtualRoot := &Rule{"virtual_root", prereqs, "# build all roots."}
+		tree.root = nodes.AddRule(virtualRoot)
+	}
+	return nodes
+}
+
+func (tree *Tree) AddRule(rule *Rule) *Node {
+	if tree.lookup[rule.target] != nil {
+		log.Printf("Duplicate rule for %s", rule.target)
+		return nil
+	}
+	node := tree.Ensure(rule.target)
+	node.rule = rule
+	for _, pre := range rule.prereqs {
+		child := tree.Ensure(pre)
+		node.children = append(node.children, child)
+		child.parents = append(child.parents, node)
+	}
+	return node
+}
+
+func (tree *Tree) Ensure(name string) *Node {
+	if tree.lookup[name] == nil {
+		tree.lookup[name] = &Node{name: name}
+	}
+	return tree.lookup[name]
+}
---
+++ jobs/jobs_test.go
@@ -0,0 +1,26 @@
+package jobs
+
+import (
+	"reflect"
+	"testing"
+)
+
+var tests []*Rule = []*Rule{
+	&Rule{target: "target",
+		prereqs: []string{"one", "two", "three"},
+		recipe:  "build1"},
+}
+
+func TestBuildTree(t *testing.T) {
+	tree := BuildTree(tests)
+	expect(t, "tree", tree.root.name, "target")
+	expect(t, "children", len(tree.root.children), 3)
+	expect(t, "parent", len(tree.Ensure("one").parents), 1)
+	expect(t, "root", tree.Ensure("one)").parents[0], tree.root)
+}
+
+func expect(t *testing.T, what string, got, expected interface{}) {
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("Expected %s == %v(%T) but got %v(%T).\n", what, expected, expected, got, got)
+	}
+}
